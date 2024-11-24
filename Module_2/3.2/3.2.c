6ebc55e51515c0ee52945891e1834ef32b9db001
данил серов <serov-danil-garena@inbox.ru>
2024-11-24 22:26:24 +0700

    +4.3 and 3.1 update

--- Module_2/3.2/3.2.c
+++ Module_2/3.2/3.2.c
@@ -1,10 +1,30 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<sys/types.h>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+#include <arpa/inet.h>
+uint32_t generate_random_ip() {
+    return (uint32_t)rand();
+}
 
 
 int main(int argc, char* argv[]){
-    u_int32_t i;
+    if (argc != 4) {
+        fprintf(stderr, "Использование: %s <IP шлюза> <маска подсети> <кол-во пакетов>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    const char* gateway_str = argv[1];
+    const char* mask_str = argv[2];
+    int num_packets = atoi(argv[3]);
+
+
+    if (num_packets <= 0 || num_packets > MAX_PACKETS) {
+        fprintf(stderr, "Ошибка: Количество пакетов должно быть положительным числом (1-%d).\n", MAX_PACKETS);
+        return EXIT_FAILURE;
+    }
+    uint32_t gateway, mask;
+
+
 
     return 0;
 }
--- Module_2/4.3/phoneBtree.c
+++ Module_2/4.3/phoneBtree.c
@@ -4,9 +4,9 @@
 #define MAX_CONTACTS 20
 #define MAX_LENGHT 25
 
-typedef struct Contact{
-    char firstname[MAX_LENGHT];
-    char secondname[MAX_LENGHT];
+typedef struct {
+    const char firstname[MAX_LENGHT];
+    const char secondname[MAX_LENGHT];
     char middlename[MAX_LENGHT];
     char place_work[MAX_LENGHT];
     char name_work[MAX_LENGHT];
@@ -15,146 +15,179 @@ typedef struct Contact{
     char link_mess[MAX_LENGHT];
     char profile[MAX_LENGHT];
 }Contact;
-typedef struct Tree Tree;
 
-typedef struct Tree{
+
+typedef struct Tree {
     Contact con;
-    Item* next;
-    Item* prev;
+    Tree* left;
+    Tree* right;
 }Tree;
-
-
-Tree* Delete_Contact(Tree* head) {
-    Tree* item = head;
-    if(item!=NULL){
-        return item;
-    }
+void deleteTree(Tree* root) {
+    if (root == NULL) return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    free(root);
 }
-Tree* Edit_contact(Tree* head,char* firstname, char* secondname,char* phone, char* link, char* place_work, char* name_work, char* email, char* profile,char* link_mess) {
-    Tree* Tree = head;
-    while(item != NULL) {
-        if (strncmp(item->con.firstname, firstname, MAX_LENGHT) == 0 && strncmp(item->con.secondname, secondname, MAX_LENGHT) == 0) {
-            strncpy(item->con.email, email, MAX_LENGHT);
-            strncpy(item->con.phone, phone, MAX_LENGHT);
-            strncpy(item->con.link_mess, link_mess, MAX_LENGHT);
-            strncpy(item->con.profile, profile, MAX_LENGHT);
-            return head;
-        }
-        item = item->next;
+
+void Edit_contact(Tree** q, char* firstname, char* secondname, char* phone, char* link, char* place_work, char* name_work, char* email, char* profile, char* link_mess) {
+    if (*q == NULL) {
+        return;
     }
+
+    if (strncmp((*q)->con.secondname, secondname, MAX_LENGHT) == 0 && strncmp((*q)->con.firstname, firstname, MAX_LENGHT) == 0) {
+        strncpy((*q)->con.email, email, MAX_LENGHT);
+        strncpy((*q)->con.phone, phone, MAX_LENGHT);
+        strncpy((*q)->con.link_mess, link_mess, MAX_LENGHT);
+        strncpy((*q)->con.profile, profile, MAX_LENGHT);
+        return;
+    }
+    if (strncmp((*q)->con.secondname, secondname, MAX_LENGHT) < 0 || (strncmp((*q)->con.secondname, secondname, MAX_LENGHT) == 0 && strncmp((*q)->con.firstname, firstname, MAX_LENGHT) < 0)) {
+        Edit_contact(&(*q)->left, firstname, secondname, phone, link, place_work, name_work, email, profile, link_mess);
+    }
+    else {
+        Edit_contact(&(*q)->right, firstname, secondname, phone, link, place_work, name_work, email, profile, link_mess);
+    }
+
     printf("net takogo");
-    return item;
+    return;
 }
 Contact create_contact(char* firstname, char* secondname, char* middlename, char* place_work,
     char* name_work,
     char* email,
     char* phone,
-    char* link_mess,char* profile) {
+    char* link_mess, char* profile) {
     Contact newcontact;
-    strncpy(newcontact.firstname, firstname,MAX_LENGHT);
-    strncpy(newcontact.secondname, secondname,MAX_LENGHT);
-    strncpy(newcontact.middlename, middlename,MAX_LENGHT);
-    strncpy(newcontact.email, email,MAX_LENGHT);
-    strncpy(newcontact.phone,phone, MAX_LENGHT);
-    strncpy(newcontact.link_mess, link_mess,MAX_LENGHT);
+    strncpy(newcontact.firstname, firstname, MAX_LENGHT);
+    strncpy(newcontact.secondname, secondname, MAX_LENGHT);
+    strncpy(newcontact.middlename, middlename, MAX_LENGHT);
+    strncpy(newcontact.email, email, MAX_LENGHT);
+    strncpy(newcontact.phone, phone, MAX_LENGHT);
+    strncpy(newcontact.link_mess, link_mess, MAX_LENGHT);
     strncpy(newcontact.profile, profile, MAX_LENGHT);
     return newcontact;
 }
 
-Item* Add_contacts(Item* head, Contact con) {
-    Item* newcontact = malloc(sizeof(Item));
-    if (!newcontact) {
-        printf("Ошибка выделения памяти\n");
-        return head;
+void Add_contacts(Tree** q, Contact con) {
+    if (*q == NULL) {
+        *q = malloc(sizeof(Tree));
+        (*q)->left = (*q)->right = NULL;
+        (*q)->con = con;
+        return;
     }
-    newcontact->con = con;
-    newcontact->next=newcontact->prev=NULL;
-    if (head == NULL) {
-        return newcontact;
+    if (strncmp((*q)->con.secondname, con.secondname, MAX_LENGHT) == 0 && strncmp((*q)->con.firstname, con.firstname, MAX_LENGHT) == 0) {
+        printf("Такой контакт уже существует");
+        return;
     }
-    Item* current = head;
-    while (current != NULL) {
-        if (strncmp(con.secondname, current->con.secondname, MAX_LENGHT) < 0 || (strncmp(con.secondname, current->con.secondname, MAX_LENGHT) == 0 && strncmp(con.firstname, current->con.firstname, MAX_LENGHT) < 0)) {
-            
-            newcontact->next = current;
-            newcontact->prev = current->prev;
-            if (current->prev) {
-                current->prev->next = newcontact;
-            }
-            else {
-                head = newcontact; 
-            }
-            current->prev = newcontact;
-            return head;
-        }
-        if (current->next == NULL) {
-            current->next = newcontact;
-            newcontact->prev = current;
-            return head;
-        }
-        current = current->next;
+    if (strncmp((*q)->con.secondname, con.secondname, MAX_LENGHT) < 0 || (strncmp((*q)->con.secondname, con.secondname, MAX_LENGHT) == 0 && strncmp((*q)->con.firstname, con.firstname, MAX_LENGHT) < 0)) {
+        Add_contacts(&(*q)->left, con);
     }
-    printf("такой контакт уже существует");
-    free(newcontact);
-    return head;
-}
-void printList(Item* head) {
-    for (Item* current = head; current != NULL; current = current->next) {
-        PrintContact(current->con);
+    else {
+        Add_contacts(&(*q)->right, con);
     }
-}
-void deleteList( Item* head) {
-    for (Item* item = head; item != NULL; item = head) {
-        head = head->next;
-        free(item);
-    }
-}
-Item* Delete_contact(Item* head, char* firstname, char* secondname) {
-    if (head == NULL){
-        return head;
-    }
-    for (Item* current = head; current != NULL; current = current->next) {
-        if (strncmp(current->con.firstname, firstname, MAX_LENGHT) == 0 && strncmp(current->con.secondname, secondname, MAX_LENGHT) == 0) {
-            
-            if (current == head) {
-                head = current->next;
-                if (head != NULL) {
-                    head->prev = NULL; 
-                }
-            }
-            else { 
-                current->prev->next = current->next;
-                if (current->next) { 
-                    current->next->prev = current->prev;
-                }
-            }
 
-            free(current); 
-            return head;
-        }
-    }
 }
-void PrintContact(Contact con){
-    printf("Фамилия: %s\nИмя:%s \nОтчество: %s\nКомпания: %s\nПрофессия: %s\nЕмаил: %s\nНомер телефона: %s\nСоцсеть: %s\nПрофиль: %s\n",con.secondname,con.firstname,con.middlename,con.place_work,con.name_work,con.email,con.phone,con.link_mess,con.profile);
-}
-int main(void) {
-    Item* head = NULL;
+Tree* deleteContact(Tree* root, const char* secondname, const char* firstname) {
+    if (root == NULL) return NULL;
 
+    if (strncmp(root->con.secondname, secondname, MAX_LENGHT) < 0 ||
+        (strncmp(root->con.secondname, secondname, MAX_LENGHT) == 0 &&
+            strncmp(root->con.firstname, firstname, MAX_LENGHT) < 0)) {
+        root->left = deleteContact(root->left, secondname, firstname);
+    }
+    else if (strncmp(root->con.secondname, secondname, MAX_LENGHT) > 0 ||
+        (strncmp(root->con.secondname, secondname, MAX_LENGHT) == 0 &&
+            strncmp(root->con.firstname, firstname, MAX_LENGHT) > 0)) {
+        root->right = deleteContact(root->right, secondname, firstname);
+    }
+    else {
+        // Найден узел для удаления
+        if (root->left == NULL) {
+            Tree* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Tree* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        // Узел имеет двух потомков
+        Tree* minNode = root->right;
+        while (minNode->left != NULL) {
+            minNode = minNode->left;
+        }
+
+        root->con = minNode->con;  // Замена на минимальный элемент
+        root->right = deleteContact(root->right, minNode->con.secondname, minNode->con.firstname);
+    }
+
+    return root;
+}
+
+void printcontact(Contact con) {
+    printf("Фамилия: %s\nИмя %s\nОтчество: %s\nEmail: %s\nСсылка на мессенджер: %s\nПрофиль %s\nТелефон: %s\nМесто работы: %s\nДолжность: %s\n", con.secondname, con.firstname, con.middlename, con.email, con.link_mess, con.profile, con.phone, con.place_work, con.name_work);
+}
+void printList(Tree* q) {
+    if (q == NULL) return;
+    printList(q->left);
+    printcontact(q->con);
+    printList(q->right);
+}
+
+
+int main() {
+    Tree* head = NULL;
+
+    // Добавление контактов
     Contact contact1 = create_contact("Иван", "Иванов", "Иванович", "Компания А", "Инженер", "ivanov@mail.com", "123456789", "messenger.com/ivanov", "profile.com/ivanov");
     Contact contact2 = create_contact("Петр", "Петров", "Петрович", "Компания Б", "Менеджер", "petrov@mail.com", "987654321", "messenger.com/petrov", "profile.com/petrov");
-    
-    head = Add_contacts(head, contact1);
-    head = Add_contacts(head, contact2);
+    Contact contact3 = create_contact("Сергей", "Сергеев", "Сергеевич", "Компания В", "Разработчик", "sergeev@mail.com", "112233445", "messenger.com/sergeev", "profile.com/sergeev");
+    Contact contact4 = create_contact("Анна", "Антонова", "Александровна", "Компания Г", "Аналитик", "antonova@mail.com", "998877665", "messenger.com/antonova", "profile.com/antonova");
+    Contact duplicateContact = create_contact("Иван", "Иванов", "Иванович", "Компания А", "Инженер", "ivanov@mail.com", "123456789", "messenger.com/ivanov", "profile.com/ivanov");
+
+    Add_contacts(&head, contact1);
+    Add_contacts(&head, contact2);
+    Add_contacts(&head, contact3);
+    Add_contacts(&head, contact4);
 
     printf("Контакты после добавления:\n");
     printList(head);
-    printf("\nРедактирование телефона Ивана Иванова:\n");
-    head = Edit_contact(head, "Иван", "Иванов", "555555555", "messenger.com/ivanov", "Компания А", "Инженер", "ivanov@mail.com", "profile.com/ivanov", "link.com/ivan");
+
+    printf("\nПопытка добавления дубликата Ивана Иванова:\n");
+    Add_contacts(&head, duplicateContact);
     printList(head);
 
-    printf("\nУдаление Петра Петрова:\n");
-    head = Delete_contact(head, "Петр", "Петров");
+    printf("\nРедактирование телефона и email Сергея Сергеева:\n");
+    Edit_contact(&head, "Сергей", "Сергеев", "554433221", "messenger.com/sergeev_new", "Компания В", "Разработчик", "sergeev_new@mail.com", "profile.com/sergeev", "link.com/sergeev");
     printList(head);
-    deleteList(head);
+
+    printf("\nПопытка редактирования несуществующего контакта (Мария Морозова):\n");
+    Edit_contact(&head, "Мария", "Морозова", "667788990", "messenger.com/morozova", "Компания Д", "Дизайнер", "morozova@mail.com", "profile.com/morozova", "link.com/morozova");
+    printList(head);
+
+    printf("\nУдаление контакта Анны Антоновой (лист):\n");
+    head = deleteContact(head, "Анна", "Антонова");
+    printList(head);
+
+    printf("\nУдаление контакта Сергея Сергеева (с одним потомком):\n");
+    head = deleteContact(head, "Сергей", "Сергеев");
+    printList(head);
+
+    printf("\nУдаление контакта Ивана Иванова (корень):\n");
+    head = deleteContact(head, "Иван", "Иванов");
+    printList(head);
+
+    printf("\nУдаление контакта Петра Петрова (последний контакт):\n");
+    head = deleteContact(head, "Петр", "Петров");
+    printList(head);
+
+    printf("\nПопытка удаления из пустого дерева:\n");
+    head = deleteContact(head, "Неизвестный", "Контакт");
+    printList(head);
+
+    printf("\nОсвобождение памяти дерева:\n");
+    deleteTree(head);
+
     return 0;
 }
