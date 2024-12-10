7bc5c880a9568aa663bb4be52a504756c4cba98f
данил серов <serov-danil-garena@inbox.ru>
2024-12-11 01:14:00 +0700

    a

--- Module_3/07/receiver.c
+++ Module_3/07/receiver.c
@@ -18,7 +18,7 @@ int main() {
 	while (1) {
 		if (mq_receive(ds, new_text, SIZE, &prio) == -1) {
 			perror("cannot receive");
-			return -1;
+			break;
 		}
 		printf("Friend: %s\n", new_text);
 		if (strcmp(new_text, "exit") == 0) {
@@ -31,7 +31,7 @@ int main() {
 		
 		if (mq_send(ds, new_text, strlen(new_text), PRIORITY) == -1) {
 			perror("Sending message error");
-			return -1;
+			break;
 		}
 		
 		if (strncmp(new_text, "exit", SIZE) == 0) {
@@ -42,7 +42,5 @@ int main() {
 	}
 	if (mq_close(ds) == -1)
 		perror("Closing queue error");
-	if (mq_unlink(QUEUE_NAME) == -1)
-		perror("Removing queue error");
 	return 0;
 }
