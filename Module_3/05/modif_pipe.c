b5726a45ed877ba374e18da65b88089508af7127
данил серов <serov-danil-garena@inbox.ru>
2025-01-02 22:47:33 +0700

    a

--- Module_3/05/modif_pipe.c
+++ Module_3/05/modif_pipe.c
@@ -7,52 +7,60 @@
 #include <fcntl.h>
 #include<stdbool.h>
 #include<signal.h>
-sem_t sem;
+volatile sig_atomic_t file_locked = false;
+void handle_sigusr1(int sig) {
+    file_locked = true;
+}
+void handle_sigusr2(int sig) {
+    file_locked = false;
+}
 
-
-
-void Parent_process(int *pipefd,int count, pid_t Child_Pid){
+void Parent_process(int* pipefd, int count, pid_t Child_Pid) {
     close(pipefd[1]);
 
-    int r =0 ;
-    for(int i=0;i<count;i++){
+    int r = 0;
+    for (int i = 0;i < count;i++) {
         sleep(3);
-        sem_post(&sem);
-        
-        if(read(pipefd[0],&r,sizeof(r))>0){
+
+        kill(Child_Pid, SIGUSR1);
+
+        if (read(pipefd[0], &r, sizeof(r)) > 0) {
             printf("Read from pipe %d\n", r);
         }
-        else{
+        else {
             perror("read");
             break;
         }
-        sem_wait(&sem);
-        
+
+        kill(Child_Pid, SIGUSR2);
+
     }
     close(pipefd[0]);
     wait(NULL);
 }
-void Child_Process(int *pipefd,int count){
-    
+void Child_Process(int* pipefd, int count) {
+    signal(SIGUSR1, handle_sigusr1);
+    signal(SIGUSR2, handle_sigusr2);
     close(pipefd[0]);
-
-    for(int i = 0; i<count;i++){
-        
-        sem_wait(&sem);
-        int r = rand()%100;
-        printf("Write in pipe: %d\n",r);
-        if(write(pipefd[1],&r,sizeof(r))==-1){
+    for (int i = 0; i < count;i++) {
+        while (file_locked) {
+            usleep(1000);
+        }
+        int r = rand() % 100;
+        printf("Write in pipe: %d\n", r);
+        if (write(pipefd[1], &r, sizeof(r)) == -1) {
             perror("write");
             close(pipefd[1]);
             exit(1);
         }
+        sleep(1);
     }
     close(pipefd[1]);
     exit(0);
 
 }
-int main(int argc, char* argv[]){
-    if(argc!= 2){
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
         perror("Enter 2 element");
         exit(EXIT_FAILURE);
     }
@@ -62,29 +70,22 @@ int main(int argc, char* argv[]){
     pid_t pid;
     int r;
     int pipefd[2];
-
-    if (sem_init(&sem, 1, 0) == -1) {
-        perror("sem_init failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if(pipe(pipefd)){
-        fprintf(stderr,"Pipe failed.\n");
+    if (pipe(pipefd)) {
+        fprintf(stderr, "Pipe failed.\n");
         return EXIT_FAILURE;
     }
     pid = fork();
-    if(pid<0){
+    if (pid < 0) {
         perror("fail fork");
         exit(EXIT_FAILURE);
     }
-    else if(pid == 0){
+    else if (pid == 0) {
         Child_Process(pipefd, count);
     }
-    else{
+    else {
         Parent_process(pipefd, count, pid);
     }
     return 0;
-    
-}
 
+}
 
