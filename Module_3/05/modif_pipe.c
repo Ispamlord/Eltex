aadded46e35c5979bded1a16eb2a719482a23ce0
данил серов <serov-danil-garena@inbox.ru>
2025-01-02 22:42:57 +0700

    08 task on check

--- Module_3/05/modif_pipe.c
+++ Module_3/05/modif_pipe.c
@@ -7,13 +7,9 @@
 #include <fcntl.h>
 #include<stdbool.h>
 #include<signal.h>
-volatile sig_atomic_t file_locked = false;
-void handle_sigusr1(int sig) {
-    file_locked = true; 
-}
-void handle_sigusr2(int sig) {
-    file_locked = false; 
-}
+sem_t sem;
+
+
 
 void Parent_process(int *pipefd,int count, pid_t Child_Pid){
     close(pipefd[1]);
@@ -21,8 +17,7 @@ void Parent_process(int *pipefd,int count, pid_t Child_Pid){
     int r =0 ;
     for(int i=0;i<count;i++){
         sleep(3);
-        
-        kill(Child_Pid, SIGUSR1);
+        sem_post(&sem);
         
         if(read(pipefd[0],&r,sizeof(r))>0){
             printf("Read from pipe %d\n", r);
@@ -31,21 +26,19 @@ void Parent_process(int *pipefd,int count, pid_t Child_Pid){
             perror("read");
             break;
         }
-        
-        kill(Child_Pid, SIGUSR2);
+        sem_wait(&sem);
         
     }
     close(pipefd[0]);
     wait(NULL);
 }
 void Child_Process(int *pipefd,int count){
-    signal(SIGUSR1, handle_sigusr1);
-    signal(SIGUSR2, handle_sigusr2);
+    
     close(pipefd[0]);
+
     for(int i = 0; i<count;i++){
-        while (file_locked) {
-            usleep(1000);
-        }
+        
+        sem_wait(&sem);
         int r = rand()%100;
         printf("Write in pipe: %d\n",r);
         if(write(pipefd[1],&r,sizeof(r))==-1){
@@ -53,7 +46,6 @@ void Child_Process(int *pipefd,int count){
             close(pipefd[1]);
             exit(1);
         }
-        sleep(1);
     }
     close(pipefd[1]);
     exit(0);
@@ -70,6 +62,12 @@ int main(int argc, char* argv[]){
     pid_t pid;
     int r;
     int pipefd[2];
+
+    if (sem_init(&sem, 1, 0) == -1) {
+        perror("sem_init failed");
+        exit(EXIT_FAILURE);
+    }
+
     if(pipe(pipefd)){
         fprintf(stderr,"Pipe failed.\n");
         return EXIT_FAILURE;
