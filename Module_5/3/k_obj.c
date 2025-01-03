0c1f3ff9f29eaaca9558ce9c39cd1dbbae39cda6
данил серов <serov-danil-garena@inbox.ru>
2025-01-04 03:35:00 +0700

    3 лаба я надеюсь уже

--- Module_5/3/k_obj.c
+++ Module_5/3/k_obj.c
@@ -6,9 +6,9 @@
 #include <linux/module.h>
 #include <linux/string.h>
 #include <linux/sysfs.h>
+#include <linux/vt.h>
 #include <linux/vt_kern.h>
-#include <linux/console.h>
-#include <linux/timer.h>
+#include <linux/console_struct.h>
 
 #define BLINK_DELAY HZ / 5
 #define ALL_LEDS_ON 0x07
@@ -21,62 +21,53 @@ static int kbledstatus = 0;
 static int value = 0;
 
 static ssize_t foo_show(struct kobject* kobj, struct kobj_attribute* attr, char* buf) {
-    return sysfs_emit(buf, "%d\n", value);
+    return sprintf(buf, "%d\n", value);
 }
 
 static ssize_t foo_store(struct kobject* kobj, struct kobj_attribute* attr, const char* buf, size_t count) {
-    int ret;
-    ret = kstrtoint(buf, 10, &value);
-    if (ret < 0)
-        return ret;
+    sscanf(buf, "%du", &value);
     return count;
 }
 
 static void my_timer_func(struct timer_list* unused) {
-    struct vc_data* vc;
-    struct tty_struct* tty;
+    struct tty_struct* t = vc_cons[fg_console].d->port.tty;
 
-    console_lock();
-    vc = console_driver->ops->get_cons(console_driver, -1);
-    tty = vc ? vc->port.tty : NULL;
+    if (kbledstatus == value)
+        kbledstatus = RESTORE_LEDS;
+    else
+        kbledstatus = value;
 
-    if (tty && tty->driver && tty->driver->ops->ioctl) {
-        kbledstatus = (kbledstatus == value) ? RESTORE_LEDS : value;
-        tty->driver->ops->ioctl(tty, KDSETLED, kbledstatus);
-    }
-    console_unlock();
+    (my_driver->ops->ioctl)(t, KDSETLED, kbledstatus);
 
-    mod_timer(&my_timer, jiffies + BLINK_DELAY);
+    my_timer.expires = jiffies + BLINK_DELAY;
+    add_timer(&my_timer);
 }
 
 static int kbleds_init(void) {
-    struct vc_data* vc;
-
-    console_lock();
-    vc = console_driver->ops->get_cons(console_driver, -1);
-    if (!vc || !vc->port.tty) {
-        console_unlock();
-        return -ENODEV;
+    int i;
+    pr_info("kbleds: loading\n");
+    pr_info("kbleds: fgconsole is %x\n", fg_console);
+    for (i = 0; i < MAX_NR_CONSOLES; i++) {
+        if (!vc_cons[i].d)
+            break;
+        pr_info("poet_atkm: console[%i/%i] #%i, tty %lx\n", i, MAX_NR_CONSOLES, vc_cons[i].d->vc_num, (unsigned long)vc_cons[i].d->port.tty);
     }
-    my_driver = vc->port.tty->driver;
-    console_unlock();
+    pr_info("kbleds: finished scanning consoles\n");
+
+    my_driver = vc_cons[fg_console].d->port.tty->driver;
+    pr_info("kbleds: tty driver name %x\n", my_driver->name);
 
     timer_setup(&my_timer, my_timer_func, 0);
-    mod_timer(&my_timer, jiffies + BLINK_DELAY);
 
+    my_timer.expires = jiffies + BLINK_DELAY;
+    add_timer(&my_timer);
     return 0;
 }
 
 static void kbleds_cleanup(void) {
-    struct vc_data* vc;
-
-    del_timer_sync(&my_timer);
-
-    console_lock();
-    vc = console_driver->ops->get_cons(console_driver, -1);
-    if (vc && vc->port.tty && my_driver->ops->ioctl)
-        my_driver->ops->ioctl(vc->port.tty, KDSETLED, RESTORE_LEDS);
-    console_unlock();
+    pr_info("kbleds: unloading...\n");
+    del_timer(&my_timer);
+    (my_driver->ops->ioctl)(vc_cons[fg_console].d->port.tty, KDSETLED, RESTORE_LEDS);
 }
 
 static struct kobj_attribute foo_attribute = __ATTR(value, 0660, foo_show, foo_store);
@@ -84,26 +75,28 @@ static struct kobj_attribute foo_attribute = __ATTR(value, 0660, foo_show, foo_s
 static int __init led_init(void) {
     int error = 0;
 
+    pr_debug("Module initialized successfully \n");
+
     example_kobject = kobject_create_and_add("sys_test", kernel_kobj);
     if (!example_kobject)
         return -ENOMEM;
 
-    error = sysfs_create_file(example_kobject, &foo_attribute.attr);
-    if (error) {
-        pr_debug("Failed to create sysfs entry\n");
+    if ((error = sysfs_create_file(example_kobject, &foo_attribute.attr))) {
+        pr_debug("failed to create the foo file in /sys/kernel/sys_test \n");
         return error;
     }
-
-    error = kbleds_init();
-    if (error)
-        pr_debug("Failed to initialize kbleds\n");
+    if ((error = kbleds_init())) {
+        pr_debug("failed kbleds_init \n");
+        return error;
+    }
 
     return error;
 }
 
 static void __exit exit_module(void) {
-    kobject_put(example_kobject);
+    pr_debug("Module un initialized successfully \n");
     kbleds_cleanup();
+    kobject_put(example_kobject);
 }
 
 MODULE_LICENSE("GPL");
