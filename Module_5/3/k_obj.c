c5d83bcf9335b00110dcd2ee95b05f497a5e74a3
данил серов <serov-danil-garena@inbox.ru>
2025-01-03 18:09:37 +0700

    3 tak 5 module

--- Module_5/3/k_obj.c
+++ Module_5/3/k_obj.c
@@ -1,76 +1,60 @@
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
+#include <linux/init.h>
+#include <linux/tty.h>
+#include <linux/kd.h>
+#include <linux/console_struct.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/sysfs.h>
 
-#define PROC_NAME "proc_example"
-
-static char* msg;
-static int len, temp;
-
-ssize_t read_proc(struct file* filp, char __user* buf, size_t count, loff_t* offp) {
-    if (*offp > 0) {
-        return 0;
-    }
-    if (count > temp) {
-        count = temp;
-    }
-    temp = temp - count;
-
-    if (copy_to_user(buf, msg, count)) {
-        return -EFAULT;
-    }
-
-    if (count == 0) {
-        temp = len;
-    }
-
-    *offp += count;
-    return count;
-}
-
-ssize_t write_proc(struct file* filp, const char __user* buf, size_t count, loff_t* offp) {
-    if (count > 100) {
-        count = 100;
-    }
-
-    if (copy_from_user(msg, buf, count)) {
-        return -EFAULT;
-    }
-
-    len = count;
-    temp = len;
-    msg[count] = '\0';
-    return count;
-}
-
-static const struct proc_ops proc_fops = {
-    .proc_read = read_proc,
-    .proc_write = write_proc,
-};
-
-void create_new_proc_entry(void) {
-    proc_create(PROC_NAME, 0666, NULL, &proc_fops);
-    msg = kmalloc(100 * sizeof(char), GFP_KERNEL);
-    if (!msg) {
-        pr_alert("Failed to allocate memory\n");
-    }
-}
-
-static int __init proc_init(void) {
-    create_new_proc_entry();
-    pr_info("proc file created\n");
-    return 0;
-}
-
-static void __exit proc_cleanup(void) {
-    remove_proc_entry(PROC_NAME, NULL);
-    kfree(msg);
-    pr_info("proc file removed\n");
-}
-
-MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Keyboard LED Firfe module 3 task");
 MODULE_AUTHOR("serov");
-module_init(proc_init);
-module_exit(proc_cleanup);
+MODULE_LICENSE("GPL");
+
+static struct tty_driver* kbled_driver;
+static char led_status = 0;
+
+#define ALL_LEDS_OFF  0x00
+
+static ssize_t kbled_write(struct kobject* kobj, struct kobj_attribute* attr,
+    const char* buf, size_t count)
+{
+    int new_value;
+    if (kstrtoint(buf, 10, &new_value))
+        return -EINVAL;
+
+    if (new_value < 0 || new_value > 7)
+        return -EINVAL;
+
+    led_status = new_value;
+    (kbled_driver->ioctl)(vc_cons[fg_console].d->vc_tty, NULL, KDSETLED, led_status);
+
+    return count;
+}
+
+static struct kobj_attribute kbled_attr = __ATTR(led_control, 0664, NULL, kbled_write);
+
+static int __init kbleds_init(void)
+{
+    int ret;
+
+    kbled_driver = vc_cons[fg_console].d->vc_tty->driver;
+
+    ret = sysfs_create_file(kernel_kobj, &kbled_attr.attr);
+    if (ret)
+        printk(KERN_ERR "Failed to create sysfs attribute\n");
+    else
+        printk(KERN_INFO "kbleds: sysfs interface created\n");
+
+    return ret;
+}
+
+static void __exit kbleds_exit(void)
+{
+    sysfs_remove_file(kernel_kobj, &kbled_attr.attr);
+    (kbled_driver->ioctl)(vc_cons[fg_console].d->vc_tty, NULL, KDSETLED, ALL_LEDS_OFF);
+    printk(KERN_INFO "kbleds: module unloaded\n");
+}
+
+module_init(kbleds_init);
+module_exit(kbleds_exit);
