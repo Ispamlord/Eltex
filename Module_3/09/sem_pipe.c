9522a13be949273944fafd432b5d88ac14066dbf
данил серов <serov-danil-garena@inbox.ru>
2025-01-07 03:35:13 +0700

    09

--- Module_3/09/sem_pipe.c
+++ Module_3/09/sem_pipe.c
@@ -10,10 +10,8 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
-int write_sem, read_sem;
-int reader_count = 0;
-int shm_id;
-int* shared_count;
+int sem_writer, sem_readers;
+int readers_count = 0;
 
 void sem_op(int sem_id, int op) {
     struct sembuf sem_buf;
@@ -23,13 +21,30 @@ void sem_op(int sem_id, int op) {
     semop(sem_id, &sem_buf, 1);
 }
 
-void Parent_process(int* pipefd, int count) {
+void start_read() {
+    sem_op(sem_readers, -1); 
+    readers_count++;
+    if (readers_count == 1) {
+        sem_op(sem_writer, -1);
+    }
+    sem_op(sem_readers, 1);
+}
+
+void stop_read() {
+    sem_op(sem_readers, -1); 
+    readers_count--;
+    if (readers_count == 0) {
+        sem_op(sem_writer, 1); 
+    }
+    sem_op(sem_readers, 1); 
+}
+
+void Parent_process(int* pipefd, int count, pid_t Child_Pid) {
     close(pipefd[1]);
     int r = 0;
 
     for (int i = 0; i < count; i++) {
-        sem_op(write_sem, -1);
-
+        start_read();
         if (read(pipefd[0], &r, sizeof(r)) > 0) {
             printf("Parent read from pipe: %d\n", r);
         }
@@ -37,8 +52,7 @@ void Parent_process(int* pipefd, int count) {
             perror("read");
             break;
         }
-
-        sem_op(write_sem, 1);
+        stop_read();
         sleep(1);
     }
 
@@ -50,119 +64,80 @@ void Child_Process(int* pipefd, int count) {
     close(pipefd[0]);
 
     for (int i = 0; i < count; i++) {
-        sem_op(write_sem, -1);
+        sem_op(sem_writer, -1); // Lock writer
 
         int r = rand() % 100;
-        printf("Write to pipe: %d\n", r);
-
+        printf("Child write to pipe: %d\n", r);
         if (write(pipefd[1], &r, sizeof(r)) == -1) {
             perror("write");
             close(pipefd[1]);
+            sem_op(sem_writer, 1);
             exit(1);
         }
 
-        sem_op(write_sem, 1);
-        sleep(1);
-    }
-    close(pipefd[1]);
-    exit(0);
-}
-
-void Reader_process(int* pipefd, int count) {
-    close(pipefd[1]);
-    int r = 0;
-
-    for (int i = 0; i < count; i++) {
-        sem_op(read_sem, -1);
-        (*shared_count)++;
-        if (*shared_count == 1) {
-            sem_op(write_sem, -1);
-        }
-        sem_op(read_sem, 1);
-
-        if (read(pipefd[0], &r, sizeof(r)) > 0) {
-            printf("Reader %d read: %d\n", getpid(), r);
-        }
-        else {
-            perror("read");
-            break;
-        }
-
-        sem_op(read_sem, -1);
-        (*shared_count)--;
-        if (*shared_count == 0) {
-            sem_op(write_sem, 1);
-        }
-        sem_op(read_sem, 1);
-
+        sem_op(sem_writer, 1); 
         sleep(1);
     }
 
-    close(pipefd[0]);
+    close(pipefd[1]);
     exit(0);
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        perror("Usage: ./program <count> <num_readers>");
+    if (argc != 2) {
+        perror("Usage: <program> <count>");
         exit(EXIT_FAILURE);
     }
 
     int count = atoi(argv[1]);
-    int num_readers = atoi(argv[2]);
-    int pipefd[2];
     pid_t pid;
+    int pipefd[2];
 
-    key_t key1 = ftok("/tmp", 'a');
-    key_t key2 = ftok("/tmp", 'b');
+    key_t key_writer = ftok("/tmp", 'a');
+    key_t key_readers = ftok("/tmp", 'b');
 
-    write_sem = semget(key1, 1, 0666 | IPC_CREAT);
-    read_sem = semget(key2, 1, 0666 | IPC_CREAT);
-    shm_id = smget(IPC_PRIVATE, sizeof(int), 0666 | IPC_CREAT);
-
-    if (write_sem == -1 || read_sem == -1 || shm_id == -1) {
-        perror("semget/shmget");
+    sem_writer = semget(key_writer, 1, 0666 | IPC_CREAT);
+    sem_readers = semget(key_readers, 1, 0666 | IPC_CREAT);
+    if (sem_writer == -1 || sem_readers == -1) {
+        perror("semget");
         exit(EXIT_FAILURE);
     }
 
-    semctl(write_sem, 0, SETVAL, 1);
-    semctl(read_sem, 0, SETVAL, 1);
-    shared_count = (int*)shmat(shm_id, NULL, 0);
-    *shared_count = 0;
+    semctl(sem_writer, 0, SETVAL, 1); 
+    semctl(sem_readers, 0, SETVAL, 1); 
 
     if (pipe(pipefd)) {
         fprintf(stderr, "Pipe failed.\n");
         return EXIT_FAILURE;
     }
 
-    pid = fork();
+    for (int i = 0; i < 3; i++) {  
+        pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0) {
+            Parent_process(pipefd, count, pid);
+            exit(0);
+        }
+    }
+
+    pid = fork(); 
     if (pid < 0) {
-        perror("fail fork");
+        perror("fork");
         exit(EXIT_FAILURE);
     }
     else if (pid == 0) {
         Child_Process(pipefd, count);
     }
 
-    for (int i = 0; i < num_readers; i++) {
-        pid = fork();
-        if (pid < 0) {
-            perror("fail fork");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0) {
-            Reader_process(pipefd, count);
-        }
-    }
-
-    for (int i = 0; i < num_readers + 1; i++) {
+    for (int i = 0; i < 4; i++) {
         wait(NULL);
     }
 
-    semctl(write_sem, 0, IPC_RMID);
-    semctl(read_sem, 0, IPC_RMID);
-    shmdt(shared_count);
-    shmctl(shm_id, IPC_RMID, NULL);
+    semctl(sem_writer, 0, IPC_RMID);
+    semctl(sem_readers, 0, IPC_RMID);
 
     return 0;
 }
