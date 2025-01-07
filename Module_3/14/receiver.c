85fbcdfc471988195b6e8e569d50095ffe4120c8
данил серов <serov-danil-garena@inbox.ru>
2025-01-07 19:50:12 +0700

    14

---
+++ Module_3/14/receiver.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t addr_len = sizeof(client_addr);
+
+    // Создание UDP сокета
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Ошибка при создании сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    // Настройка адреса для прослушивания
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // Привязка сокета
+    if (bind(sockfd, (const struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Ошибка при привязке сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Ожидание сообщений...\n");
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+
+        // Получение сообщения
+        recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+            (struct sockaddr*)&client_addr, &addr_len);
+        printf("Получено: %s", buffer);
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Чат завершен.\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
---
+++ Module_3/14/sender.c
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t addr_len = sizeof(server_addr);
+
+    // Создание UDP сокета
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Ошибка при создании сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    // Настройка адреса сервера
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    printf("Отправитель готов. Введите сообщение:\n");
+
+    while (1) {
+        fgets(buffer, BUFFER_SIZE, stdin);  // Чтение строки из терминала
+
+        // Отправка сообщения серверу
+        sendto(sockfd, buffer, strlen(buffer), 0,
+            (struct sockaddr*)&server_addr, addr_len);
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Чат завершен.\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
