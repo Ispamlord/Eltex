9620c4a10d2756634854cc3ce8682f1caf1a39f7
данил серов <serov-danil-garena@inbox.ru>
2025-01-07 20:07:05 +0700

    14 final

--- Module_3/14/sender.c
+++ Module_3/14/sender.c
@@ -13,7 +13,6 @@ int sockfd;
 struct sockaddr_in peer_addr;
 socklen_t addr_len = sizeof(peer_addr);
 
-// Поток для приема сообщений
 void* receive_messages(void* arg) {
     char buffer[BUFFER_SIZE];
     while (1) {
@@ -27,38 +26,32 @@ int main() {
     struct sockaddr_in my_addr;
     char buffer[BUFFER_SIZE];
 
-    // Создание сокета
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Ошибка при создании сокета");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка адреса для приема
     memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
     my_addr.sin_addr.s_addr = INADDR_ANY;
     my_addr.sin_port = htons(PORT1);
 
-    // Привязка сокета
     if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) {
         perror("Ошибка при привязке");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка адреса собеседника
     memset(&peer_addr, 0, sizeof(peer_addr));
     peer_addr.sin_family = AF_INET;
     peer_addr.sin_port = htons(PORT2);
     peer_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    // Поток для приема сообщений
     pthread_t recv_thread;
     pthread_create(&recv_thread, NULL, receive_messages, NULL);
 
     printf("Клиент 1 запущен. Введите сообщение:\n");
 
-    // Основной цикл отправки
     while (1) {
         fgets(buffer, BUFFER_SIZE, stdin);
         sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&peer_addr, addr_len);
