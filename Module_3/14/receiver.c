6509cc02baf9c423ffcfeb3c3142185803770920
данил серов <serov-danil-garena@inbox.ru>
2025-01-07 20:05:26 +0700

    14

--- Module_3/14/receiver.c
+++ Module_3/14/receiver.c
@@ -2,31 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
-#define PORT1 8080  // Порт клиента 1
-#define PORT2 9090  // Порт клиента 2
+#define PORT1 8080
+#define PORT2 9090
 #define BUFFER_SIZE 1024
 
+int sockfd;
+struct sockaddr_in peer_addr;
+socklen_t addr_len = sizeof(peer_addr);
+
+// Поток для приема сообщений
+void* receive_messages(void* arg) {
+    char buffer[BUFFER_SIZE];
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&peer_addr, &addr_len);
+        printf("\nОтвет: %s", buffer);
+    }
+}
+
 int main() {
-    int sockfd;
-    struct sockaddr_in my_addr, peer_addr;
+    struct sockaddr_in my_addr;
     char buffer[BUFFER_SIZE];
 
     // Создание сокета
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
         perror("Ошибка при создании сокета");
         exit(EXIT_FAILURE);
     }
 
-    // Настройка собственного адреса
+    // Настройка адреса для приема
     memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
     my_addr.sin_addr.s_addr = INADDR_ANY;
     my_addr.sin_port = htons(PORT2);
 
-    // Привязка к порту
-    if (bind(sockfd, (const struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) {
+    // Привязка сокета
+    if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) {
         perror("Ошибка при привязке");
         exit(EXIT_FAILURE);
     }
@@ -37,25 +52,20 @@ int main() {
     peer_addr.sin_port = htons(PORT1);
     peer_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    // Поток для приема сообщений
+    pthread_t recv_thread;
+    pthread_create(&recv_thread, NULL, receive_messages, NULL);
+
     printf("Клиент 2 запущен. Введите сообщение:\n");
 
+    // Основной цикл отправки
     while (1) {
         fgets(buffer, BUFFER_SIZE, stdin);
-
-        // Отправка сообщения клиенту 1
-        sendto(sockfd, buffer, strlen(buffer), 0,
-            (struct sockaddr*)&peer_addr, sizeof(peer_addr));
-
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&peer_addr, addr_len);
         if (strncmp(buffer, "exit", 4) == 0) {
             printf("Выход.\n");
             break;
         }
-
-
-        // Получение ответа
-        memset(buffer, 0, BUFFER_SIZE);
-        recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-        printf("Ответ: %s", buffer);
     }
 
     close(sockfd);
