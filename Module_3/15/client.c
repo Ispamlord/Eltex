19a2e10af907b3caaefc4d60d797d630ace69d2f
данил серов <serov-danil-garena@inbox.ru>
2025-01-07 20:17:15 +0700

    15

---
+++ Module_3/15/client.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = { 0 };
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Неверный адрес");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Ошибка подключения");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Подключено к серверу. Введите запрос (пример: 10 + 5):\n");
+
+    while (1) {
+        fgets(buffer, BUFFER_SIZE, stdin);
+        send(sock, buffer, strlen(buffer), 0);
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Отключение...\n");
+            break;
+        }
+
+        memset(buffer, 0, BUFFER_SIZE);
+        read(sock, buffer, BUFFER_SIZE);
+        printf("%s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
