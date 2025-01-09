9cb8bc16388767a3b5fd486ca874d234a8332565
данил серов <serov-danil-garena@inbox.ru>
2025-01-10 01:58:55 +0700

    16

---
+++ Module_3/16/client.c
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void send_file(int sock, const char* filename) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("Ошибка открытия файла");
+        return;
+    }
+
+    // Сообщаем серверу о передаче файла
+    send(sock, "file", 4, 0);
+    send(sock, filename, strlen(filename), 0);
+    sleep(1);
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, file)) > 0) {
+        send(sock, buffer, bytes_read, 0);
+    }
+
+    fclose(file);
+    printf("Файл %s отправлен серверу.\n", filename);
+}
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = { 0 };
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Неверный адрес");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Ошибка подключения");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Подключено к серверу.\n");
+    printf("Введите запрос (пример: 10 + 5) или 'file [имя файла]':\n");
+
+    while (1) {
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        // Проверка на команду передачи файла
+        if (strncmp(buffer, "file", 4) == 0) {
+            char filename[BUFFER_SIZE];
+            sscanf(buffer + 5, "%s", filename);
+            send_file(sock, filename);
+            continue;
+        }
+
+        send(sock, buffer, strlen(buffer), 0);
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Отключение...\n");
+            break;
+        }
+
+        memset(buffer, 0, BUFFER_SIZE);
+        read(sock, buffer, BUFFER_SIZE);
+        printf("%s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ Module_3/16/server.c
@@ -0,0 +1,122 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void process_request(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    int num1, num2;
+    char op;
+    double result;
+    FILE* file;
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        read(client_socket, buffer, BUFFER_SIZE);
+
+        // Проверка на передачу файла
+        if (strncmp(buffer, "file", 4) == 0) {
+            char filename[BUFFER_SIZE];
+            read(client_socket, filename, BUFFER_SIZE);
+            file = fopen(filename, "wb");
+            if (file == NULL) {
+                perror("Ошибка открытия файла");
+                continue;
+            }
+            printf("Прием файла: %s\n", filename);
+
+            ssize_t bytes_read;
+            while ((bytes_read = read(client_socket, buffer, BUFFER_SIZE)) > 0) {
+                fwrite(buffer, 1, bytes_read, file);
+                if (bytes_read < BUFFER_SIZE) break;
+            }
+
+            fclose(file);
+            printf("Файл %s успешно получен.\n", filename);
+            continue;
+        }
+
+        if (strncmp(buffer, "exit", 4) == 0) {
+            printf("Клиент отключился.\n");
+            break;
+        }
+
+        // Обработка математических операций
+        sscanf(buffer, "%d %c %d", &num1, &op, &num2);
+
+        switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = (double)num1 / num2;
+            else {
+                strcpy(buffer, "Ошибка: деление на ноль.");
+                send(client_socket, buffer, strlen(buffer), 0);
+                continue;
+            }
+            break;
+        default:
+            strcpy(buffer, "Ошибка: неизвестная операция.");
+            send(client_socket, buffer, strlen(buffer), 0);
+            continue;
+        }
+
+        snprintf(buffer, BUFFER_SIZE, "Результат: %.2f", result);
+        send(client_socket, buffer, strlen(buffer), 0);
+    }
+
+    close(client_socket);
+}
+
+int main() {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Ошибка привязки");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Ошибка прослушивания");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Ожидание подключения клиента...\n");
+
+    while (1) {
+        client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        if (client_socket < 0) {
+            perror("Ошибка подключения");
+            exit(EXIT_FAILURE);
+        }
+        printf("Клиент подключен.\n");
+
+        process_request(client_socket);
+    }
+
+    return 0;
+}
